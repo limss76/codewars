e0bbb039ceee4256e5381fc61745b9a9b4af8fb7
Halim <halim06sumbogo07@gmail.com>
2022-03-24 11:51:35 +0700

    Codewars | Double Char | 8Kyu

---
+++ javascript/Double_Char/solutions.js
@@ -0,0 +1,15 @@
+function doubleChar(str) {
+  return str
+    .split("")
+    .map(function (c) {
+      return c + c;
+    })
+    .join("");
+}
+
+console.log(doubleChar("abcd"), "aabbccdd");
+console.log(doubleChar("Adidas"), "AAddiiddaass");
+console.log(doubleChar("1337"), "11333377");
+console.log(doubleChar("illuminati"), "iilllluummiinnaattii");
+console.log(doubleChar("123456"), "112233445566");
+console.log(doubleChar("%^&*("), "%%^^&&**((");
